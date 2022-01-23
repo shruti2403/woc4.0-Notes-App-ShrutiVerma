b98cd2ad775a44acbc47b6f19eca3a8e54472732
vshruti24 <202103027@daiict.ac.in>
2022-01-23 23:05:54 +0530

    COMMIT

--- notes1.java
+++ notes1.java
@@ -1,15 +1,18 @@
-.function Add()
+function Add(Note//#endregion)
 {
      var list2 = document.getElementById("list2");
-     list2.style.display="inline-block";
+     list2.style.display = "inline-block";
      var x = prompt('Name','Note');
      if (x !=null)
-     {
+     
          document.getElementById("2").innerHTML = x;
-     } 
+      
 }
-.function Delete()
+function Delete(Note//#endregion*/)
 {
     var list2=document.getElementById("list2");
     list2.style.display="none";
 }
+static getElementById()
+
+ 
