38d977fd316112867012f667a393614049fb3a49
vshruti24 <202103027@daiict.ac.in>
2022-01-28 23:04:52 +0530

    commit

--- notes1.java
+++ notes1.java
@@ -1,18 +1,18 @@
-function Add(Note//#endregion)
-{
-     var list2 = document.getElementById("list2");
-     list2.style.display = "inline-block";
-     var x = prompt('Name','Note');
-     if (x !=null)
+// function Add(Note//#endregion)
+// {
+//      var list2 = document.getElementById("list2");
+//      list2.style.display = "inline-block";
+//      var x = prompt('Name','Note');
+//      if (x !=null)
      
-         document.getElementById("2").innerHTML = x;
+//          document.getElementById("2").innerHTML = x;
       
-}
-function Delete(Note//#endregion*/)
-{
-    var list2=document.getElementById("list2");
-    list2.style.display="none";
-}
-static getElementById()
-
+// }
+// function Delete(Note//#endregion*/)
+// {
+//     var list2=document.getElementById("list2");
+//     list2.style.display="none";
+// }
+// static getElementById()
+     let Name = prompt ("What is your name","Guest");
  
